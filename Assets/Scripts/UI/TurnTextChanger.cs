3d0a06e8b584b194a22380b3e267b113a2d6316a
Макс Філіпов <maksfilipov05@gmail.com>
2023-03-16 21:05:47 +0200

    Create project

---
+++ Assets/Scripts/UI/TurnTextChanger.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TurnTextChanger : MonoBehaviour
+{
+    private TextMeshProUGUI turnText;
+    private Animator textAnimator;
+
+    private void Start()
+    {
+        turnText = GetComponent<TextMeshProUGUI>();
+        textAnimator = GetComponent<Animator>();
+    }
+
+    public void ChangeTurnText(PawnColor pawnColor)
+    {
+        turnText.text = pawnColor.ToString().ToUpper() + "'S TURN";
+        textAnimator.SetTrigger("NextTurn");
+        turnText.color = pawnColor == PawnColor.White ? Color.white : Color.black;
+    }
+}
